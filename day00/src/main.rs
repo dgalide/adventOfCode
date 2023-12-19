5270f176d33e1ad9a1e9861c16b886090d2e5474
Damien <damien.galidie.externe@emeria.eu>
2023-12-19 21:34:24 +0100

    day00

--- day00/src/main.rs
+++ day00/src/main.rs
@@ -1,9 +1,46 @@
 use std::fs;
 
 fn main() {
-    let file_path = "example.txt";
+    let file_path = "input.txt";
 
-    let contents = fs::read_to_string(file_path);
+    let contents = fs::read_to_string(file_path).unwrap();
 
-    println!("With text:\n{:?}", contents);
+    let splitted = contents.split("\n");
+
+    let mut total = 0;
+
+    splitted.for_each(|line| {
+        let mut first_digit: Option<char> = None;
+        let mut last_digit: Option<char> = None;
+
+        if line.is_empty() {
+            return;
+        }
+        line.chars().for_each(|c| {
+            if c.is_digit(10) {
+                if first_digit.is_none() {
+                    first_digit = Some(c);
+                }
+                last_digit = Some(c);
+            }
+        });
+
+        if last_digit.is_none() {
+            return;
+        }
+
+        let combined = format!("{}{}", first_digit.unwrap(), last_digit.unwrap());
+
+        let as_number = combined.parse::<u32>();
+
+        if as_number.is_err() {
+            return;
+        }
+
+        let as_number = as_number.unwrap();
+
+        total += as_number;
+    });
+
+    println!("Total: {}", total);
 }
